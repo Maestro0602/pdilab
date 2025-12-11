4950d8a62d4f63da521742c40081db2fc2188727
OukPuthirith <oukputhirith@gmail.com>
2025-12-11 23:38:47 +0700

    lab6 changes

--- Ex2/Main.java
+++ Ex2/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args){
         //Polymorphism method
         //Java still calls the correct overriden class, based on the actual object type at runtime;
-        Employee E = new Employee("Horng",20);
-        Employee M = new Manager("Naco", 30);
-        Employee I = new Intern("Bot", 40);
+        Employee E = new Employee("Heang",20);
+        Employee M = new Manager("Heng", 30);
+        Employee I = new Intern("Lyda", 40);
 
         
         E.calculate_bonus();
--- Ex5/Main.java
+++ Ex5/Main.java
@@ -2,7 +2,7 @@ package Ex5;
 
 public class Main {
     public static void main(String[] args){
-        Book b = new Book("Stranger Things","p20240017","kal");
+        Book b = new Book("harry Potty","p20240033","Rith");
         b.display_info();
 
         System.out.println(b.title);
